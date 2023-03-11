d38c0d9361a282add5d01f9f5ba4644c3683a81d
Taha Amin Ghafuri <82469904+tahaghafuri@users.noreply.github.com>
2023-03-11 10:08:41 +0330

    Fixing The Helper view/make Directory Bug

--- bootstrap/helper.php
+++ bootstrap/helper.php
@@ -3,8 +3,8 @@
 use Devinow\Blade\Blade;
 
 function view($path, array $data = []){
-    $view = __DIR__.'/../../resources/views';
-    $cache = __DIR__.'/../../storage/cache';
+    $view = __DIR__.'/../resources/views';
+    $cache = __DIR__.'/../storage/cache';
 
     $blade = new Blade($view, $cache);
     echo $blade->view()->make($path, $data)->render();
@@ -15,7 +15,7 @@ function make($filename, $data){
 
     ob_start();
 
-    include __DIR__.'/../../resources/mails/'.$filename.'.php';
+    include __DIR__.'/../resources/mails/'.$filename.'.php';
 
     $content=ob_get_contents();
 
