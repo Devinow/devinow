f9327ee4e01bdd5a46430eaca81ed4cbf40dd564
Taha Amin Ghafuri <tahaghafuri@gmail.com>
2023-03-11 14:54:32 +0330

    Update Redirect Class

--- app/classes/redirect.php
+++ app/classes/redirect.php
@@ -5,11 +5,11 @@ namespace App\Classes;
 
 class Redirect{
     public static function to($page){
-        return header('Location: '.$page);
+        return redirect($page);
     }
 
     public static function back(){
-        return header('Location: '.$_SERVER['REQUEST_URI']);
+        return redirect($_SERVER['REQUEST_URI']);
     }
 }
 
