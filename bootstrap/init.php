6e7a94d9013b1ee8983c38308f458e1c82e798b5
Taha Amin Ghafuri <tahaghafuri@gmail.com>
2023-03-14 21:11:59 +0330

    Optimize ORM Config And Remove Database Class

--- bootstrap/init.php
+++ bootstrap/init.php
@@ -6,15 +6,7 @@ if(!isset($_SESSION)) session_start();
 require_once __DIR__.'/../config/_env.php';
 
 //Instantiate Database Class
-$dataSource = new \Devinow\Db\PdoDataSource('mysql');
-$dataSource->setHostname($_ENV['DB_HOST']);
-$dataSource->setPort(3306);
-$dataSource->setDatabaseName($_ENV['DB_NAME']);
-$dataSource->setCharset('utf8mb4');
-$dataSource->setUsername($_ENV['DB_USERNAME']);
-$dataSource->setPassword($_ENV['DB_PASSWORD']);
-$GLOBALS['db'] = \Devinow\Db\PdoDatabase::fromDataSource($dataSource);
-unset($dataSource);
+require_once __DIR__.'/../config/_db.php';
 
 //Set Custom Error Handler
 set_error_handler([new \Core\Classes\Errorhandler(), 'handleErrors']);
